1d4aebd8cd3b9c485501d057b134b59ee1365c3c
Aleksey V Zapparov <ixti@member.fsf.org>
2012-02-20 17:14:15 +0100

    Finished lexer. Now it generates correct events.

--- test/nrouter/route-test.js
+++ test/nrouter/route-test.js
@@ -10,13 +10,9 @@ var tmp;
 tmp = new Route('');
 tmp = new Route('/article/{category.slug}');
 tmp = new Route('/article/{category.slug}/\\{id}-{id}');
-//tmp = new Route('/article/{category.slug}/{id}(.html)');
-//tmp = new Route('/article/{category.slug}/{id}(-{slug})(.{format})');
-//tmp = new Route('/article/{category.slug}/{id}\\{hehe}(-{slug})(.{format})');
-
-/*
-*/
-
+tmp = new Route('/article/{category.slug}/{id}(.html)');
+tmp = new Route('/article/{category.slug}/{id}(-{slug})(.{format})');
+tmp = new Route('/article/{category.slug}/{id}\\{hehe}(-{slug})(.{format})');
 
 require('vows').describe('NRouter.Route').addBatch({
   'need tests': 'tdb'

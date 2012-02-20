06160603d226e774d015509a153e7793ade0800a
Aleksey V Zapparov <ixti@member.fsf.org>
2012-02-20 16:07:51 +0100

    Make parser recognize escaped {}

--- test/nrouter/route-test.js
+++ test/nrouter/route-test.js
@@ -6,7 +6,16 @@ var Route = require('../../lib/nrouter/route');
 
 
 // tmp measure to test parser
-var tmp = new Route('/article/{category.slug}/{id}(-{slug})(.{format})');
+var tmp;
+tmp = new Route('');
+tmp = new Route('/article/{category.slug}');
+tmp = new Route('/article/{category.slug}/\\{id}-{id}');
+//tmp = new Route('/article/{category.slug}/{id}(.html)');
+//tmp = new Route('/article/{category.slug}/{id}(-{slug})(.{format})');
+//tmp = new Route('/article/{category.slug}/{id}\\{hehe}(-{slug})(.{format})');
+
+/*
+*/
 
 
 require('vows').describe('NRouter.Route').addBatch({

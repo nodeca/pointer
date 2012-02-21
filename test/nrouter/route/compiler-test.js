881118d4e3f5e3a3a645e2924cb4016ee93a2538
Aleksey V Zapparov <ixti@member.fsf.org>
2012-02-21 19:40:22 +0100

    Improve compiler test

--- test/nrouter/route/compiler-test.js
+++ test/nrouter/route/compiler-test.js
@@ -6,22 +6,22 @@ var Compiler = require('../../../lib/nrouter/route/compiler');
 var Common = require('../../../lib/nrouter/common');
 
 
+var token_map = {string: 's', param: 'p', metachar: 'm'};
 
 // creates a map of node types
 function map_nodes(nodes) {
   var map = [];
 
   nodes.forEach(function (node) {
-    map.push(
-      ('optional' === node.type) ? map_nodes(node.nodes)
-                                 : {'string': 's', 'param': 'p'}[node.type]
-    );
+    var ch = token_map[node.type] || '!';
+    map.push(('optional' === node.type) ? map_nodes(node.nodes) : ch);
   });
 
   return map;
 }
 
 
+// create compiler test
 function test_compiler(definitions) {
   var tests = {};
 
@@ -37,19 +37,34 @@ function test_compiler(definitions) {
 
 
 require('vows').describe('NRouter.Route.Compiler').addBatch({
-  'Compiling route into AST': test_compiler({
+  'Compiling routes into AST': test_compiler({
     '': [],
     '/foo': ['s'],
     '/foo/{bar}': ['s','p'],
     '/foo(/{bar})': ['s',['s','p']],
     '/foo/{bar}(-{baz})(/(({deep}sheep)))': ['s','p',['s','p'],['s',[['p','s']]]],
 
-    // testing escaping
+    // metachars
+
+    '/foo/{bar}.html': ['s','p','s'],
+    '/foo/{bar}?html': ['s','p','m','s'],
+    '/foo/{bar}*html': ['s','p','m','s'],
+    '/foo/{bar}+html': ['s','p','m','s'],
+
+    // escaping
+
     '/foo\\{test}': ['s'],
     '/foo{test\\}more}': ['s','p'],
     '/foo\\(test)': ['s'],
     '/foo(test\\)more)': ['s',['s']],
     '/foo(test\\)(inner)more)': ['s',['s',['s'],'s']],
-    '/foo/\\({a}(-{b}\\))crazzy': ['s','p',['s','p','s'],'s']
+    '/foo/\\({a}(-{b}\\))crazzy': ['s','p',['s','p','s'],'s'],
+
+    // escaping metachars
+
+    '/foo/{bar}\\.html': ['s','p','s'],
+    '/foo/{bar}\\?html': ['s','p','s'],
+    '/foo/{bar}\\*html': ['s','p','s'],
+    '/foo/{bar}\\+html': ['s','p','s']
   })
 }).export(module);
